b0f193ab0dcc9b9a033c233ead1b12ec6c9fbd0b
Tim Caswell <tim.caswell@palm.com>
2011-04-28 11:37:25 -0700

    Move http connection logic into library
    Change-Id: I7cd00f40ad32864b8536c70d6d31c2f331bfaf50

--- sampleapp/app.js
+++ sampleapp/app.js
@@ -1,26 +1,11 @@
 #!/usr/bin/env node
 
-var Http = require('http'),
-    Stack = require('stack'),
+var Stack = require('stack'),
     Creationix = require('creationix'),
     TopCube = require('topcube');
 
-var PORT = 7569;
-var handler = Stack(
+TopCube(Stack(
   Creationix.log(),
-  Creationix.static("/", __dirname + "/www")
-);
-(function listen() {
-  try {
-    Http.createServer(handler).listen(PORT);
-  } catch (err) {
-    if (err.code === "EADDRINUSE") {
-      PORT++;
-      listen();
-      return;
-    }
-    throw err;
-  }
-  TopCube("http://127.0.0.1:" + PORT + "/index.html");
-}());
+  Creationix.static("/", __dirname + "/www", "index.html")
+));
 
--- topcube.js
+++ topcube.js
@@ -1,12 +1,27 @@
-var modpath = __dirname + "/build/default/topcube_native.node";
-var ChildProcess = require('child_process');
-module.exports = function WebApp(url) {
-  // TODO: Fix native bindings to not block the node event loop so this won't be needed
+var ChildProcess = require('child_process'),
+    Http = require('http');
+
+module.exports = function WebApp(handler) {
+
+  var PORT = 7569;
+  (function listen() {
+    try {
+      Http.createServer(handler).listen(PORT);
+    } catch (err) {
+      if (err.code === "EADDRINUSE") {
+        PORT++;
+        listen();
+        return;
+      }
+      throw err;
+    }
+  }());
+
+  var url = "http://127.0.0.1:" + PORT + "/";
   var child = ChildProcess.spawn(process.execPath, [__dirname + "/client.js", url]);
   child.on('exit', function (code) {
     process.exit(code);
   });
   child.stdout.pipe(process.stdout);
   child.stderr.pipe(process.stderr);
-  return child;
 };
