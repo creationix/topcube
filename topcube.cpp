91e8cb40cd8f768521a7ebd0e784f0c2e3243fb8
Young Hahn <young@developmentseed.org>
2012-01-09 12:45:59 -0500

    Open new windows in default browser.

--- topcube.cpp
+++ topcube.cpp
@@ -3,6 +3,13 @@
 #include <gtk/gtk.h>
 #include <webkit/webkitwebview.h>
 
+// stl for n00b debugging :)
+#include <sstream>
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+// std::clog << "debug output";
+
 using namespace node;
 using namespace v8;
 
@@ -17,7 +24,19 @@ void destroy (void)
 
 void title_change (void)
 {
-	gtk_window_set_title(GTK_WINDOW (window), webkit_web_view_get_title(WEBKIT_WEB_VIEW (web_view)));
+  gtk_window_set_title(GTK_WINDOW (window), webkit_web_view_get_title(WEBKIT_WEB_VIEW (web_view)));
+}
+
+void new_window (
+  WebKitWebView *web_view,
+  WebKitWebFrame *frame,
+  WebKitNetworkRequest *request)
+{
+  gchar *argv[3];
+  argv[0] = const_cast<char*>("/usr/bin/xdg-open");
+  argv[1] = const_cast<char*>(webkit_network_request_get_uri(request));
+  argv[2] = NULL;
+  g_spawn_async(NULL, argv, NULL, G_SPAWN_LEAVE_DESCRIPTORS_OPEN, NULL, NULL, NULL, NULL);
 }
 
 static Handle<Value> create_window(const Arguments& args)
@@ -49,10 +68,11 @@ static Handle<Value> create_window(const Arguments& args)
 
   gtk_signal_connect (GTK_OBJECT (window), "destroy", GTK_SIGNAL_FUNC (destroy), NULL);
   gtk_signal_connect (GTK_OBJECT (web_view), "title-changed", GTK_SIGNAL_FUNC (title_change), NULL);
+  gtk_signal_connect (GTK_OBJECT (web_view), "new-window-policy-decision-requested", GTK_SIGNAL_FUNC (new_window), NULL);
 
   gtk_container_add (GTK_CONTAINER (scrolled_window), web_view);
   gtk_container_add (GTK_CONTAINER (window), scrolled_window);
-  
+
   webkit_web_view_load_uri (WEBKIT_WEB_VIEW (web_view), *url);
 
   gtk_window_set_default_size (GTK_WINDOW (window), width, height);
@@ -60,7 +80,6 @@ static Handle<Value> create_window(const Arguments& args)
 
   // TODO: find a way to not block the node event loop
   gtk_main ();
-
   return Undefined();
 
 }
