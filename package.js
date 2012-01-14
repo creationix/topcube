37406c36f6feada39001cf406317dc95574e8a24
Young Hahn <young@developmentseed.org>
2012-01-14 15:46:50 -0500

    Update install wrapper.

--- package.js
+++ package.js
@@ -2,15 +2,21 @@ var exec = require('child_process').exec;
 switch (process.platform) {
 case 'win32':
     console.log('win32: Downloading CEF client...');
-    exec('curl -skL https://github.com/downloads/mapbox/cefclient/cefclient-0.0.1.tar.gz | tar zx', function(err) {
-        if (err) return process.exit(1);
-        console.log('win32: CEF client downloaded successfully.')
-        process.exit(0);
+    exec('curl -skL https://github.com/downloads/mapbox/cefclient/cefclient-0.0.1.tar.gz | tar zx', function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        return err ? process.exit(1) : process.exit(0);
+    });
+    break;
+case 'linux':
+    exec('node-waf clean || true; node-waf configure build', function(err, stdout, stderr) {
+        console.log(stdout);
+        console.log(stderr);
+        return err ? process.exit(1) : process.exit(0);
     });
     break;
 default:
-    exec('node-waf clean || true; node-waf configure build', function(err) {
-        if (err) return process.exit(1);
-        process.exit(0);
-    });
+    console.log('Topcube currently does not support %s', process.platform);
+    return process.exit(0);
+    break;
 }
