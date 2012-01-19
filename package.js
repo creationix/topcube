8d9cf48a8be634284169f3a2e770452d0dea712b
= <=>
2012-01-19 13:30:06 -0500

    Update topcube download url.

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 switch (process.platform) {
 case 'win32':
     console.log('win32: Downloading CEF client...');
-    exec('curl -skL https://github.com/downloads/mapbox/cefclient/cefclient-0.0.1.tar.gz | tar zx', function(err, stdout, stderr) {
+    exec('curl -skL https://github.com/downloads/mapbox/cefclient/cefclient-0.0.2.tar.gz | tar zx', function(err, stdout, stderr) {
         console.log(stdout);
         console.log(stderr);
         return err ? process.exit(1) : process.exit(0);
